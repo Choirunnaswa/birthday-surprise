78cc4eeab8cad2d22827af7a516c950f7d6a9f12
Choirunnaswa <naswaalayubi@gmail.com>
2026-08-23 23:56:43 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-  title: "Happy Birthday, Nur Ayu Faisah ❤️",
-  description: "A special birthday gift for Nur Ayu Faisah ❤️",
+  title: "Happy Birthday, Arya Agung Saputra ❤️",
+  description: "A special birthday gift for Aryaaa ❤️",
 };
 
 export default function RootLayout({
